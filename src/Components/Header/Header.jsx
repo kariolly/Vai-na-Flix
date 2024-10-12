94a2592603157a10257cb2cb10fe2b76890d837c
karine <karioliveirap@gmail.com>
2024-10-12 17:46:04 -0300

    primeiro commit

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./HeaderStyle.scss";
+
+function Header() {
+    return(
+        <header>
+            <nav>
+                <ul>
+                    <li>Sobre</li>
+                    <li>Contato</li>
+                    <li>Saiba mais</li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default Header
---
+++ src/Components/Main/Main.jsx
@@ -0,0 +1,57 @@
+import "./MainStyle.scss"
+export default function Main() {
+    return (
+        <main>
+            <h1>🍿VAI NA FLIX</h1>
+
+            <section>
+                <figure>
+                    <div>
+                        <img src="https://image.tmdb.org/t/p/original/uyvGIduQXz1XovDeUuykmOJFqzw.jpg" alt="karate kid"/>
+                        <h2>The Karate Kid</h2>
+                        <p>Dre Parker (Jaden Smith) se mudou com a mãe (Taraji P. Henson) para Pequim, devido ao trabalho dela. Logo ao chegar ele se interessa por Meiying (Han Wenwen), uma garota que conhece praticando violino na praça. A aproximação deles provoca a irritação de Cheng (Zhenwei Wang), que lhe dá uma surra usando a técnica do kung fu. A partir de então a vida de Dre se torna um inferno, já que passa a ser perseguido na escola por Cheng e seus colegas. Um dia, ao escapar deles, Dre é auxiliado pelo sr. Han (Jackie Chan), o zelador de seu prédio, que é também um mestre de kung fu.</p>
+                    </div>
+
+                    <div>
+                        <img src="https://disneyplusbrasil.com.br/wp-content/uploads/2024/03/Divertida-Mente-2-Poster.jpg" alt="divertidamente"/>
+                        <h2>DivertidaMente 2</h2>
+                        <p>Quando Riley (Kaitlyn Dias) chega a tão temida adolescência as já conhecidas, Alegria (Amy Poehler), Tristeza (Phyllis Smith), Raiva, Medo (Tony Hale) e Nojinho (Liza Lapira), que há muito tempo administram uma operação bem-sucedida, não têm certeza de como se sentir quando novos inquilinos chegam ao local, sendo um deles a tão temida Ansiedade (Maya Hawke).</p>
+                    </div>
+                   
+                    <div
+                        ><img src="https://wallpapercave.com/wp/wp3998470.jpg" alt="green book" />
+                        <h2>Green Book</h2>
+                        <p>Em Green Book - O Guia, Tony Lip (Viggo Mortensen), um dos maiores fanfarrões de Nova York, precisa de trabalho após sua discoteca, o Copacabana, fechar as portas. Ele conhece um um pianista e quer que Lip faça uma turnê com ele. Enquanto os dois se chocam no início, um vínculo finalmente cresce à medida que eles viajam.</p>
+                    </div>
+                   
+                </figure>
+
+
+                <figure>
+                    <div>
+                        <img src="https://www.cafecomfilme.com.br/media/k2/items/cache/6b4b922a74fca14aa855a2064cdfc23c_M.jpg?t=20210322_162547" alt="coringa" />
+                        <h2>Coringa 2</h2>
+                        <p>Preso no hospital psiquiátrico de Arkham, ele acaba conhecendo Harleen "Lee" Quinzel (Lady Gaga). A curiosidade mútua acaba se transformando em paixão e obsessão e eles desenvolvem um relacionamento romântico e doentio. Lee e Arthur embarcam em uma desventura alucinada, fervorosa e musical pelo submundo de Gotham City, enquanto o julgamento público d'O Coringa se desenrola, impactando toda a cidade e suas próprias mentes conturbadas.</p>
+                    </div>
+
+                    <div>
+                        <img src="https://www.verdadeluz.com.br/wp-content/uploads/2014/11/20249359.jpg" alt="amor alem da vida" />
+                        <h2>Amor alem da vida</h2>
+                        <p>Chris Nielsen (Robin Williams), Annie (Annabella Sciorra), sua esposa, e os filhos do casal fazem uma família feliz. Mas os jovens morrem em um acidente e o casal é bastante afetado, principalmente Annie. No entanto, eles superam a morte dos filhos e conseguem levar suas vidas adiante, mas quatro anos depois é a vez de Chris morrer em um acidente e ser mandado para o Paraíso. Enquanto tenta entender o Paraíso, onde tudo pode acontecer, bastando que apenas deseje realmente, Chris fica sabendo que Annie, dominada pela dor, comete suicídio. Assim, ele nunca poderá encontrá-la, pois os suicidas são mandados para outro lugar. Mesmo assim decide tentar achá-la, apesar de ser avisado que mesmo que a encontre, ela nunca o reconhecerá.</p>
+                    </div>
+
+                    <div>
+                        <img src="https://s2.glbimg.com/p86RKh0xwquHCziW3sg0q9OvDAs=/smart/e.glbimg.com/og/ed/f/original/2019/05/21/the-art-of-racing-in-the-rain-poster-768x1137.jpg" alt="the art of racing in the rain" />
+                        <h2>Meu amigo Enzo</h2>
+                        <p>Em Meu Amigo, Denny Swift (Milo Ventimiglia) é um piloto de corridas arrojado, com um talento especial para dirigir sob chuva. Um dia, ao ir para o trabalho, encontra um filhote de cachorro que decide adotar. Ele ganha o nome de Enzo, em homenagem ao criador da Ferrari, e passa a acompanhá-lo em todo lugar que vá, ganhando um apreço especial pela adrenalina das corridas, seja ao assisti-las ao vivo ou pela televisão. Com o passar dos anos, a amizade entre Denny e Enzo sofre profundas mudanças quando o piloto conhece, e se apaixona, por Eve (Amanda Seyfried).</p>
+                    </div>
+                     
+                </figure>
+
+                   
+                
+            </section>
+            
+        </main>
+    )
+}
